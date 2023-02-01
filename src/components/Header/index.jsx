fbc338300e48975c1239e94e560f787e67f0b256
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2023-02-01 13:20:09 +0200

    header added

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from '../Button'
+import Nav from '../Nav'
+import s from './index.module.css'
+import logo from '../../media/logo_site.png'
+import {BsGlobe2} from 'react-icons/bs'
+export default function Header() {
+
+const sales = 'Sales:\n+1 (800) 315-5939'
+
+  return (
+    <header className={s.main_header}>
+        <div>
+            <Nav/>
+            <img src={logo} alt="logo" />
+            <div className={s.info_block}>
+                <p>Sales: <br />+1 (800) 315-5939</p>
+                <BsGlobe2 className={s.icon_glob}/>
+                <p>Search</p>
+                <p>login</p>
+            </div>
+            <Button>Sign up</Button>
+        </div>
+    </header>
+  )
+}
